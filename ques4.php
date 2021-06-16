a3a24ff188bd1c802cd181337910963e3483efb4
thananajem13 <72042799+thananajem13@users.noreply.github.com>
2021-06-16 23:31:56 +0300

    first task

---
+++ ques4.php
@@ -0,0 +1,17 @@
+
+<?php
+/* start forth ques. */
+// using while
+$counter=0;
+while($counter<10){
+    echo "hello world"."<br>";
+    $counter++;
+}
+// using for
+for($counter2 =0;$counter2<10;$counter2++){
+    echo "hello world"."<br>";
+}
+
+/* end forth ques. */
+
+?>
